8f8a85815444be89a2b699085b0f2fa1696bafc4
sally.sun <sally.sun@visionblue.onmicrosoft.com>
2023-10-10 04:08:03 +0200

    add
    Signed-off-by: sally.sun <sally.sun@visionblue.onmicrosoft.com>

---
+++ READMD.md
@@ -0,0 +1,8 @@
+﻿使用Topshelf 搭建 Windows 服务
+将控制台程序搭建成Windows 服务
+
+1.
+按下Win+R键,打开运行窗口;
+2.
+输入services.msc并点击确定,打开服务管理器
+3.可以找到自己开发的服务
---
+++ TopshelfDemo/Service.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopshelfDemo
+{
+    public class Service
+    {
+        public void Start()
+        {
+            //To do something
+        }
+        public void Stop()
+        {
+            //To do something
+        }
+    }
+}
